c968bbf32b5f1def4d7dfd4b5557e456d517af0d
waranya083 <117327659+waranya083@users.noreply.github.com>
2024-08-09 10:17:24 +0700

    1

---
+++ index.js
@@ -0,0 +1,6 @@
+function calculate() {
+    var x = document.getElementById("num1").value;
+    var y = document.getElementById("num2").value;
+    var z = x * y;
+    document.getElementById("result").innerHTML = z;
+}
